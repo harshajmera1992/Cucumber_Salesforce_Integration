9379459700263e7749a1c40effc2473d823f1ebb
Harsh Ajmera <32696902+harshajmera1992@users.noreply.github.com>
2020-02-04 14:44:21 +0530

    Update ExecuteMain.java

--- src/main/java/com/mkyong/rest/ExecuteMain.java
+++ src/main/java/com/mkyong/rest/ExecuteMain.java
@@ -7,7 +7,7 @@ public class ExecuteMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		NetClientPost.netClientPost();
+		//NetClientPost.netClientPost();
 		NetClientGet.netClientGet();
 	}
 }
